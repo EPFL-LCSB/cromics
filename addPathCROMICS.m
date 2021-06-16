bdc664fb362e3907422b508b553b843d4e9bfaf1
angelesmartinez <liliana.angelesmartinez@epfl.ch>
2021-06-16 17:16:59 +0200

    Set path

---
+++ addPathCROMICS.m
@@ -0,0 +1,21 @@
+%% Setting up the paths 
+
+%Add the path to CROMICS
+addpath(genpath('/Users/liliana/Dropbox/CROMICScode_PLOS/')); 
+
+
+
+% %If you want to use thermo-curated GEMs and TFA, install matTFA (https://github.com/EPFL-LCSB/matTFA)
+% %prior running CROMICS,and add the path to matTFA like  
+% addpath(genpath('/Users/liliana/GIT_Folders/matTFA/')); 
+% %Select CPLEX as solver and add the path like
+% addpath(genpath('/Users/liliana/Applications/IBM/ILOG/CPLEX_Studio1271/cplex/matlab'));
+% changeCobraSolver('cplex_direct'); 
+
+
+% %If you want to use GEMs and flux balance analysis (FBA)
+% %add the path to COBRA toolbox like  
+% addpath(genpath('/Users/liliana/cobratoolbox')); 
+% %Select CPLEX as solver and add the path like
+% addpath(genpath('/Users/liliana/Applications/IBM/ILOG/CPLEX_Studio1271/cplex/matlab'));
+% changeCobraSolver('cplex_direct'); 
